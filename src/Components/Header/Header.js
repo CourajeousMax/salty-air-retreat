01f75f8aa9fd196fe40a41bf25d14a3171d45b76
Stephany Munoz <stephany.munoz8@gmail.com>
2023-08-29 20:42:20 -0400

    deleted files, moved images to public folder

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import heroImage from '../../../public/assets/images/retreat_cave.jpg'
+import React from "react";
+import Image from "next/image";
 const Header = () => {
-    return (
-        <div className='hero'>
-            <img className = "hero__image" src= {heroImage} />
-        </div>
-    );
+  return (
+    <div className="hero">
+      <Image className="hero__image" src="/retreat_cave.jpg" width={129} height={77} />
+      <h1>Welcome to your spiritual home</h1>
+    </div>
+  );
 };
 
 export default Header;
