33c04de2b2f79880087369d412cbf982dcdd402b
Stephany Munoz <stephany.munoz8@gmail.com>
2023-09-19 20:34:58 -0400

    reorganized code and implementing button

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Image from "next/image";
+import Stars from "../../../public/stars.jpg";
+import Sunrise from "../../../public/sunrise.jpg";
+import Plants from "../../../public/plants.jpg";
 const Main = () => {
+  const pics = [Stars, Sunrise, Plants];
+  const picNames = ["Stars", "Sunrise", "Plants"];
+  
   return (
     <>
       <main>
         <section className="info">
-          <div className="info__container">
-            <div className="info__text-container">
-              <p className="info__text-one info__mobile">Classes & Events</p>
+          {["Classes & Events", "Guidance & Mentorship", "Start your spiritual journey"].map((text, index) => (
+            <div className="info__container">
+              <div className="info__text-container">
+                <Image src={pics[index]} alt={picNames[index]} className="info__mobile" width={200} height={420} />
+                <p className={`info__mobile-${index + 1} info__text`}>{text}</p>
+              </div>
             </div>
-          </div>
-          <div className="info__container">
-            <div className="info__text-container">
-              <p className="info__text-two info__mobile">Guidance & Mentorship</p>
-            </div>
-          </div>
-          <div className="info__container">
-            <div className="info__text-container">
-              <p className="info__text-three info__mobile">Start your spiritual journey</p>
-            </div>
-          </div>
+          ))}
         </section>
         <section className="benefits">
           <div className="benefits__subheader-container">
@@ -36,10 +35,8 @@ const Main = () => {
                 dust, allergens, and other pollutants. As the lamp heats up from the incandescent light source, the trapped particles remain locked in
                 the salt crystal. This purification process significantly reduces allergy symptoms and can even benefit individuals with asthma.
               </p>
-            </div>
-            <div className="benefits__image-container">
               <Image className="benefits__image" src="/cave_class.jpg" width={129} height={77} />
-              <p>
+              <p className="benefits__text">
                 Moreover, HSL release negative ions, which promote healthier breathing by enhancing cilia activity and filtering foreign particles,
                 potentially safeguarding against airborne germs. These negative ions can also boost energy levels, akin to the invigorating effects of
                 spending time in natural settings rich in negative ions, counteracting the draining impact of positive ions on our bodies.{" "}
@@ -60,36 +57,31 @@ const Main = () => {
           <p className="story__address">Located at:12307 SW 224th St, Miami, FL 33170 </p>
           <section className="cauley">
             <section className="cauley__gallery">
-              <Image className="cauley__image--one" src="/cauley-square.jpg" width={200} height={150} />
-              <Image className="cauley__image--two" src="/salty_retreat_house.jpg" width={200} height={150} />
-              <Image className="cauley__image--three" src="/interior-cave.jpg" width={200} height={150} />
-              <Image className="cauley__image--three" src="/retreat_cave.jpg" width={200} height={150} />
+              {["/cauley-square.jpg", "/salty_retreat_house.jpg", "/interior-cave.jpg", "/retreat_cave.jpg"].map((src, index) => (
+                <Image key={index} className={`cauley__image--${index + 1}`} src={src} width={200} height={150} />
+              ))}
             </section>
           </section>
-          <section className="instructor">
-            <div className="instructor__title-container">
-              <h3 className="iinstructor__title">Your reiki master teacher and guide</h3>
-              <h2 className="instructor__title">MJ Gonzalez</h2>
-            </div>
+          <article className="instructor">
+            <h3 className="instructor__title">Your reiki master teacher and guide</h3>
+            <h2 className="instructor__title">MJ Gonzalez</h2>
             <div className="instructor__container">
-              <div className="instructor__container-image">
-                <Image className="instructor__image" src="/mj.JPG" width={200} height={300} />
-                <p className="instructor__image-text">
+              <Image className="instructor__image" src="/mj.JPG" width={200} height={300} />
+              <div className="instructor__about">
+                <p className="instructor__text">
                   {" "}
                   Rev. MJ Gonzalez started growing into her spirituality by the age of 6 when she sawher first apparition standing behind a friend at
                   her favored Creek she would visit daily behind her childhood home. She was given, by the Divine, all the Clairs at a very young age.
                   As she grew, the Clairs grew stronger.{" "}
                 </p>
-              </div>
-              <div className="instructor__about">
                 <p className="instructore__text">Her spiritual modalities are, but not limted to:</p>
                 <ul className="instructor__list">
-                  <li>Clairsentience</li>
-                  <li>Clairvoyance</li>
-                  <li>Clairaudience</li>
-                  <li>Claircognizance</li>
-                  <li>Clairalience</li>
-                  <li>Clairgustance</li>
+                  {" "}
+                  <ul className="instructor__list">
+                    {["Clairsentience", "Clairvoyance", "Clairaudience", "Claircognizance", "Clairalience", "Clairgustance"].map((clair, index) => (
+                      <li key={index}>{clair}</li>
+                    ))}
+                  </ul>
                 </ul>
                 <p className="instructor__text">
                   Home, Office, and Vehicle Energy vehicle removal, Cleanser and Protection Gridding (entities, spirits, ghosts, energes, implant and
@@ -98,30 +90,37 @@ const Main = () => {
                 <p className="instructor__text">Personal Manifestations and Home/Business Gridder, Candle Imagery Reader, Energy Orb Reader</p>
                 <p className="instructor__text">Energist(Capable of holding up to 100 people energy for ceremonies)</p>
                 <ul className="instructor__list">
-                  <li>Aura Reader and Teacher</li>
-                  <li>Reiki Master Therapist of Light Therapy XII</li>
-                  <li>Spiritual Interpretation Reader</li>
-                  <li>Pendulum Practitioner</li>
-                  <li>Intuitive Practitioner</li>
-                  <li>Spiritual Surgery</li>
-                  <li>Energetics System Practitioner</li>
-                  <li>Ionic Foot Detox</li>
-                  <li>Infrared Detox</li>
+                  {[
+                    "Aura Reader and Teacher",
+                    "Reiki Master Therapist of Light Therapy XII",
+                    "Spiritual Interpretation Reader",
+                    "Pendulum Practitioner",
+                    "Intuitive Practitioner",
+                    "Spiritual Surgery",
+                    "Energetics System Practitioner",
+                    "Ionic Foot Detox",
+                    "Infrared Detox",
+                  ].map((modality, index) => (
+                    <li key={index}>{modality}</li>
+                  ))}
                 </ul>
               </div>
             </div>
-          </section>
-          <section className="events">
-            <div>
-              <div>
-                <h3>Events</h3>
-                <h2> Join our spiritual community</h2>
-                <p>See what we're up to</p>
-              </div>
-            </div>
-          </section>
+          </article>
         </div>
       </main>
+      <section className="events">
+        <div>
+          <div className="events__container">
+            <h3>Events</h3>
+            <h2> Join our spiritual community</h2>
+            <p>See what we're up to</p>
+            <div className="events__container-info">
+              <button className="events__button">Events </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
