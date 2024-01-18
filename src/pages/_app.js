bcc793bacc530d44e32f39b6ee7a46dff771f4ad
Stephany Munoz <stephany.munoz8@gmail.com>
2024-01-18 17:05:35 -0500

    fix build issue related pages/theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
-import '@/styles/globals.scss'
+import "@/styles/globals.scss";
 import { ThemeProvider } from "@mui/material/styles";
-import '../styles/globals.scss'
+import "../styles/globals.scss";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./theme";
-export default function App({ Component, pageProps}) {
+import theme from "../theme/main";
+export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
